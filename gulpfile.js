baa3525e364c34b09934c4b19891720cd3bb9560
drozdtatsiana <drozdtatsiana0@gmail.com>
2026-09-25 22:00:45 +0300

    сборка CSS с медиазапросами

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const { src, dest, watch, series, parallel } = require('gulp');
 
+
 const scss = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
 const concat = require('gulp-concat');
@@ -35,6 +36,7 @@ function fonts() {
     .pipe(dest('app/fonts'))
 }
 
+
 function images() {
   return src(['app/images/src/*.*', '!app/images/src/*.svg'])
     .pipe(newer('app/images'))
@@ -51,13 +53,14 @@ function images() {
     .pipe(dest('app/images'))
 }
 
+
 function styles() {
   return src('app/scss/*.scss')
-    .pipe(scss({ style: 'compressed' }))
-    .pipe(concat('style.min.css'))
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 10 versions']
     }))
+    .pipe(concat('style.min.css'))
+    .pipe(scss({ style: 'compressed' }))
     .pipe(dest('app/css'))
     .pipe(browserSync.stream())
 }
@@ -88,33 +91,21 @@ function watching() {
 }
 
 function cleanDist() {
-  return src('dist', { allowEmpty: true })   // ← добавил allowEmpty, чтобы не падало, если dist нет
-    .pipe(clean())
+  return src('dist')
+  .pipe(clean())
 }
 
-// 🔧 ИСПРАВЛЕНО: теперь в dist/ попадают вложенные папки (sprite и т.д.)
 function building() {
   return src([
-    'app/**/*.html',
+    'app/*.html',
     'app/js/main.min.js',
     'app/css/style.min.css',
-    'app/images/**/*',
-    'app/fonts/**/*.woff2'
-  ], { base: 'app' })
-    .pipe(dest('dist'))
+    'app/images/*.*',
+    'app/fonts/*.woff2'
+  ], {base: 'app'})
+  .pipe(dest('dist'))
 }
 
-// ✅ НОВОЕ: очистка папки docs
-function cleanDocs() {
-  return src('docs', { allowEmpty: true })
-    .pipe(clean());
-}
-
-// ✅ НОВОЕ: копирование dist → docs
-function buildDocs() {
-  return src('dist/**/*', { base: 'dist' })
-    .pipe(dest('docs'));
-}
 
 exports.styles = styles;
 exports.watching = watching;
@@ -125,12 +116,6 @@ exports.sprites = sprites;
 exports.pages = pages;
 exports.cleanDist = cleanDist;
 exports.building = building;
-exports.cleanDocs = cleanDocs;
-exports.buildDocs = buildDocs;
 
 exports.build = series(cleanDist, building);
-
-// ✅ НОВОЕ: полный деплой одной командой
-exports.deploy = series(cleanDist, building, cleanDocs, buildDocs);
-
 exports.default = parallel(styles, images, sprites, scripts, pages, watching);
