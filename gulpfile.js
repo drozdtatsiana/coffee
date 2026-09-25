37db5a668d74c939f40373677189d201c3191115
drozdtatsiana <drozdtatsiana0@gmail.com>
2026-09-25 19:07:56 +0300

    downgrade del to v6, fix footer background image

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
 const { src, dest, watch, series, parallel } = require('gulp');
-
-
 const scss = require('gulp-sass')(require('sass'));
 const browserSync = require('browser-sync').create();
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify-es').default;
 const autoprefixer = require('gulp-autoprefixer');
-const clean = require('gulp-clean');
+const del = require('del');
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 const avif = require('gulp-avif');
@@ -14,6 +12,7 @@ const newer = require('gulp-newer');
 const ttf2woff2 = require('gulp-ttf2woff2');
 const include = require('gulp-include');
 const svgstore = require('gulp-svgstore');
+
 
 function sprites() {
   return src('app/images/sprite/*.svg')
@@ -36,7 +35,6 @@ function fonts() {
     .pipe(dest('app/fonts'))
 }
 
-
 function images() {
   return src(['app/images/src/*.*', '!app/images/src/*.svg'])
     .pipe(newer('app/images'))
@@ -53,14 +51,13 @@ function images() {
     .pipe(dest('app/images'))
 }
 
-
 function styles() {
   return src('app/scss/*.scss')
+    .pipe(scss({ style: 'compressed' }))
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 10 versions']
     }))
     .pipe(concat('style.min.css'))
-    .pipe(scss({ style: 'compressed' }))
     .pipe(dest('app/css'))
     .pipe(browserSync.stream())
 }
@@ -91,8 +88,7 @@ function watching() {
 }
 
 function cleanDist() {
-  return src('dist')
-  .pipe(clean())
+  return del('docs');
 }
 
 function building() {
@@ -100,10 +96,10 @@ function building() {
     'app/*.html',
     'app/js/main.min.js',
     'app/css/style.min.css',
-    'app/images/*.*',
+    'app/images/**/*.*',
     'app/fonts/*.woff2'
-  ], {base: 'app'})
-  .pipe(dest('dist'))
+  ], { base: 'app' })
+    .pipe(dest('docs'))
 }
 
 
